0352af70f4b4244d325e844b1ae0351251446384
bwong199 <bwong199@my.bcit.ca>
2016-04-30 16:39:58 -0600

    Added Initial Entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,27 +83,46 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+
     describe("Initial Entries", function(){
+         /* TODO: Write a new test suite named "Initial Entries" */
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        var feed;
         beforeEach(function(done) {
-            loadFeed(0, function() {
-                feed = $('.feed').html();
-                loadFeed(1, done);
-            });
+            loadFeed(0, done);
+
         });
-    /* TODO: Write a new test suite named "New Feed Selection"
+
+         var feed = $('feed');
+         it('expects at least a single .entry element', function(){
+            expect($('.feed').length).not.toBe(0);
+         })
+
+    })
+
+
+
+    describe("New Feed Selection", function(){
+
+        /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var feed;
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                feed = $('.feed').html();
+                loadFeed(1, done);
+                // expect($('.entry')).toBe(>1);
+            });
+        });
+
 
         it('is loaded and the content actually changes', function() {
             expect($('.feed').html()).not.toEqual(feed);
